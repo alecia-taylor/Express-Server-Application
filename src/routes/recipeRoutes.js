5d17e9b943efb342072f9d773287de10f3c5672c
A. Taylor <alecia.taylor0@gmail.com>
2025-04-14 05:32:07 +0000

    Add recipe input validation middleware and logger; refactor recipe routes

--- src/routes/recipeRoutes.js
+++ src/routes/recipeRoutes.js
@@ -1,6 +1,13 @@
 import express from 'express';
-import * as recipeControllers from '../controllers/recipeControllers.js';
+import * as recipeController from '../controllers/recipeController.js';
+// import { validateRecipeInput } from '../middleware/validateInput.js';
 
 const router = express.Router();
-router.get('/', recipeControllers.index);
+
+router.get('/', recipeController.index);
+router.get('/:id', recipeController.show);
+router.post('/', validateRecipeInput, recipeController.create);
+router.patch('/:id', recipeController.update);
+router.delete('/:id', recipeController.destroy);
+
 export default router;
